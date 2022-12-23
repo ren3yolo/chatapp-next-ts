11a7d207f71dab3855267ace6b58a5d2e2d51036
Raj Shekhar <channiecx954@gmail.com>
2022-12-24 00:49:57 +0530

    Hardcode server url

--- socket/create.ts
+++ socket/create.ts
@@ -3,7 +3,7 @@ import { io } from "socket.io-client";
 //autoConnect can be set to true if we want to establish a connection right away. Anyway, we can call socket.connect() later.
 const socket = io(
   process.env.NODE_ENV === "production"
-    ? process.env.SERVER_URL!
+    ? "https://chatpp-server.onrender.com"
     : "http://localhost:8000",
   { autoConnect: false }
 );
